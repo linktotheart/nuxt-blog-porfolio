4ad4b11c3fdc487fc7aff592f06cde9289e12493
linktotheart <pulkitsingh302@gmail.com>
2024-07-16 15:45:47 +0530

    added about page

--- pages/about.vue
+++ pages/about.vue
@@ -4,6 +4,7 @@
         <p class="text-3xl lg:text-4xl font-bold leading-5 col-span-2 tracking-widest  uppercase">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, iusto amet. Perspiciatis error esse obcaecati? Sit ad fuga iure distinctio quidem ea nihil quam voluptatem eaque, dolores provident cupiditate dolorem.
         </p>
+        
         <figure class="flex"><img src="@/assets/images/avatar.png" class="block max-w-sm lg:max-w-full mx-auto" alt="Avatar"></figure>
     </section>
 </template>
